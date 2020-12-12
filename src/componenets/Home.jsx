32f0ae97959a30ced9476d341385a586b58119d4
Sarah <sarah.alhuriz@hotmail.com>
2020-12-11 20:51:08 -0700

    restart set up

--- src/componenets/Home.jsx
+++ src/componenets/Home.jsx
@@ -1,14 +1,9 @@
 import React, { Component } from 'react'
 
-import template from '../templates/Card'
-import settings from '../settings'
-import Patient from '../Patient'
 
 export default class Home extends Component {
-    constructor(element) {
-        super(element, template)
-        this.api = new InfermedicaApi(settings['app-id'], settings['app-key'])
-        this.patient = new Patient()
+    constructor() {
+        super()
     }
     render() {
         return (
