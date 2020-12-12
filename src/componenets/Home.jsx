af9b5a50fc4f638c8da8e8a38d5086fe6e555264
Sarah <sarah.alhuriz@hotmail.com>
2020-12-12 13:11:43 -0700

    linked symptoms to home page

--- src/componenets/Home.jsx
+++ src/componenets/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 
 export default class Home extends Component {
@@ -8,7 +9,9 @@ export default class Home extends Component {
                 <div className="row">
                     <div className="col">
                         <h2>Hello! How are you feeling today?</h2>
-                        <p className="lead">I am a very intelligent bot, I can help you find a diagnosis and connect you to a specialist</p>
+                        <p className="lead">I am a very intelligent bot</p><br/>
+                        <p>I can help you find a diagnosis and connect you to a specialist</p><br/>
+                        <Link className="link link-lg" to={`/symptoms`}>Symptom Evaluation</Link>
                     </div>
                 </div>
             </div>
