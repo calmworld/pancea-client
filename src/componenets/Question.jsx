edbe01dc577180515c4bc36fc2047feb8f2113d2
Sarah <sarah.alhuriz@hotmail.com>
2020-12-14 10:11:11 -0700

    render single groupsingle and group multi in question

--- src/componenets/Question.jsx
+++ src/componenets/Question.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import Single from './Single'
+import GroupSingle from './GroupSingle'
+import GroupMultiple from './GroupMultiple'
 
 export default class Question extends Component {
     constructor(props) {
@@ -7,11 +10,33 @@ export default class Question extends Component {
 			symptoms: [],
 			newSymptoms: [],
         }
+        this.handleQuestion = this.handleQuestion.bind(this)
     }
+
+    handleQuestion() {
+        this.props.callbackDiagnosis(this.state.newSymptoms)
+    }
+
     render() {
+        let question = this.props.question
+        let questionType
+        switch(question.type) {
+			case 'single':
+				questionType = <Single items={question.items} callbackQuestion={this.handleQuestion} />;
+				break;
+			case 'group_single':
+				questionType = <GroupSingle items={question.items} callbackQuestion={this.handleQuestion} />;
+				break;
+			case 'group_multiple':
+				questionType = <GroupMultiple items={question.items} callbackQuestion={this.handleQuestion} />;
+				break;
+			default:
+		}
         return (
-            <div>
-                
+            <div className="container">
+                <h4>Question</h4>
+                <p className="lead">{question}</p>
+				{questionType}          
             </div>
         )
     }
