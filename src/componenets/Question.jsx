c497615a7f9886e41a0a3eb9d21beb9beaf20297
Sarah <sarah.alhuriz@hotmail.com>
2020-12-14 10:04:08 -0700

    create question component

---
+++ src/componenets/Question.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Question extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+			symptoms: [],
+			newSymptoms: [],
+        }
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
