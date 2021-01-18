a9c64607709a14867c98b4bf4650138dc3c96c88
Sarah <sarah.alhuriz@hotmail.com>
2021-01-17 20:06:13 -0700

    adjust home

--- src/componenets/Home.jsx
+++ src/componenets/Home.jsx
@@ -17,7 +17,7 @@ export default class Home extends Component {
         return (
             <div className="container">
                 <h2>Hello! I am Pancea</h2>
-                <p className="lead">I can help you find a diagnosis and connect you to a specialist</p>
+                <p className="lead">I can help you find a diagnosis for your symptoms!</p>
                 <button>
                     <Link className="link link-lg" to={`/symptoms`}>Evaluate My Symptoms</Link>
                 </button>
